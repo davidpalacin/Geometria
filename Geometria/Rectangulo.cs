8acd52075352f5e207b84cd6ae7734c254db5834
davidpalacin <palacingimenezdavid.fpllefia@gmail.com>
2025-02-11 11:16:02 +0100

    Terminar Clase Rectangulo

--- Geometria/Rectangulo.cs
+++ Geometria/Rectangulo.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace Geometria
 {
-    internal class Rectangulo
+    internal class Rectangulo : Forma
     {
+        // debe tener una base y una altura, además de un método para calcular el área y otro para calcular el perimetro
+        private double b;
+        private double a;
+
+        public Rectangulo(double b, double a)
+        {
+            this.b = b;
+            this.a = a;
+        }
+
+        public double Area()
+        {
+            return this.b * this.a;
+        }
+
+        public double Perimetro()
+        {
+            return 2 * (this.b + this.a);
+        }
     }
 }
